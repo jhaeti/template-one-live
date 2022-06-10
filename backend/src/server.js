cd5c01eee3d1f4b16f3cecc62b05aa9cad7ba14e
Ti Jhae <jhaeti@gmail.com>
2022-06-10 12:42:02 +0000

    Not really any change just for testing workflow

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,4 +2,4 @@ const app = require("./app");
 
 const port = process.env.PORT;
 
-app.listen(port, console.log(`Server started on port ${port}`));
+app.listen(port, console.log(`Server started on port: ${port}`));
--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -3,7 +3,7 @@ import Layout from "../components/Layout";
 
 const about = ({ items }) => (
 	<Layout title="About the Boilerplate" description="About the boilerplate">
-		<div>About</div>
+		<div>About Page</div>
 		{items && items.map((item) => <h3 key={item.id}>{item.name}</h3>)}
 	</Layout>
 );
