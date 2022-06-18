b1ca55afaf51e638bc45600e7fd50d44717f86a8
Ti Jhae <jhaeti@gmail.com>
2022-06-18 14:26:26 +0000

    Initial commit for admin-page dashboard

---
+++ frontend/src/pages/admin-panel.js
@@ -0,0 +1,10 @@
+import Admin from "../components/Admin";
+import Layout from "../components/Layout";
+
+const adminPanel = () => (
+	<Layout title="admin-panel" description="Index for the Boilerplate">
+		<Admin />
+	</Layout>
+);
+
+export default adminPanel;
