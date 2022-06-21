5967dda2c705d0be7e9ecb265b51acf5ee8c09d3
Ti Jhae <jhaeti@gmail.com>
2022-06-21 15:53:57 +0000

    Added redirecting of all non-admin access to admin-panel to home

--- frontend/src/components/Admin.js
+++ frontend/src/components/Admin.js
@@ -1,43 +1,49 @@
 import Link from "next/link";
-
 import useFetch from "../hooks/useFetch";
 import apiUrl from "../controllers/apiUrl";
 
-const Admin = () => {
+const Admin = ({ role }) => {
 	const { data: itemsCount, error: itemsError } = useFetch(
 		`${apiUrl}/admin/items-count`
 	);
 	const { data: usersCount, error: usersError } = useFetch(
 		`${apiUrl}/admin/users-count`
 	);
-	return (
-		<div className="container admin-panel">
-			<div className="row row--center row--v-center col-fill-between">
-				<Link href="/products-page">
-					<a className="product-box">
-						<div className="row row--center row--v-center">
-							<div>
-								{(!itemsCount && !itemsError && "Loading") ||
-									itemsCount}{" "}
-								Products
+	if (role === "ADMIN") {
+		return (
+			<div className="container admin-panel">
+				<div className="row row--center row--v-center col-fill-between">
+					<Link href="/products-page">
+						<a className="product-box">
+							<div className="row row--center row--v-center">
+								<div>
+									{(!itemsCount &&
+										!itemsError &&
+										"Loading") ||
+										itemsCount}{" "}
+									Products
+								</div>
 							</div>
-						</div>
-					</a>
-				</Link>
-				<Link href="/users-page">
-					<a className="user-box">
-						<div className="row row--center row--v-center">
-							<div>
-								{(!usersCount && !usersError && "Loading") ||
-									usersCount}{" "}
-								Users
+						</a>
+					</Link>
+					<Link href="/users-page">
+						<a className="user-box">
+							<div className="row row--center row--v-center">
+								<div>
+									{(!usersCount &&
+										!usersError &&
+										"Loading") ||
+										usersCount}{" "}
+									Users
+								</div>
 							</div>
-						</div>
-					</a>
-				</Link>
+						</a>
+					</Link>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
+	return "";
 };
 
 export default Admin;
--- frontend/src/pages/admin-panel.js
+++ frontend/src/pages/admin-panel.js
@@ -1,10 +1,44 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { connect } from "react-redux";
+
+import useFetch from "../hooks/useFetch";
+
 import Admin from "../components/Admin";
 import Layout from "../components/Layout";
 
-const adminPanel = () => (
-	<Layout title="admin-panel" description="Index for the Boilerplate">
-		<Admin />
-	</Layout>
-);
+import { loadUser } from "../redux/actions/userAction";
+import apiUrl from "../controllers/apiUrl";
 
-export default adminPanel;
+const AdminPanel = ({ role, loadUser }) => {
+	const { data: userData, error } = useFetch(`${apiUrl}/users/me`);
+
+	// This is used to redirect non-admin access to admin-panel back to the index if they try accessing the admin-page by typing it in the address
+	const router = useRouter();
+	useEffect(() => {
+		if (Object.keys(router.components).length === 2) {
+			if (error || (userData && userData.user.role !== "ADMIN")) {
+				router.push("/");
+			} else if (userData) {
+				loadUser(userData);
+			}
+		}
+	}, [userData]);
+
+	return (
+		<Layout title="admin-panel" description="Index for the Boilerplate">
+			<Admin role={role} />
+		</Layout>
+	);
+};
+
+const mapStateToProps = (state) => {
+	if (!state.auth.user) {
+		return { role: "SCAM" };
+	}
+	return { role: state.auth.user.role };
+};
+
+const mapDispatchToProps = { loadUser };
+
+export default connect(mapStateToProps, mapDispatchToProps)(AdminPanel);
