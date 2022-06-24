ae84bdbb1b432113fad9a533dc3ab139bdd6f61b
Ti Jhae <jhaeti@gmail.com>
2022-06-24 00:43:58 +0000

    Created hook to redirect non-admin access to admin-only pages to homepage

---
+++ frontend/src/hooks/useCheckAdminUserOnRefresh.js
@@ -0,0 +1,19 @@
+import useFetch from "./useFetch";
+import { loadUser } from "../redux/actions/userAction";
+
+// Redirect non admin users trying to access the admin only pages to the home page
+const useCheckAdminUserOnRefresh = async (router, dispatch) => {
+	const { data, error } = await useFetch("/api/users/me");
+
+	// Object.Keys(router.components) === 2 returns true if the page was reload or access directly from the address
+	if (Object.keys(router.components).length === 2) {
+		if (error || (data && data.user.role !== "ADMIN")) {
+			router.push("/");
+		} else if (data) {
+			// Data is loaded into redux from cache if user actually is an admin but simply doing a refresh
+			dispatch(loadUser(data));
+		}
+	}
+};
+
+export default useCheckAdminUserOnRefresh;
