0dfa4ddca8fc1230b3cec1fec1bfd79b36e2095b
Ti Jhae <jhaeti@gmail.com>
2022-06-25 22:40:33 +0000

    Rename products-management page to admin-product-management

---
+++ frontend/src/pages/admin-products-management.js
@@ -0,0 +1,25 @@
+import { useRouter } from "next/router";
+import { useDispatch } from "react-redux";
+
+import Layout from "../components/Layout";
+import Operations from "../components/products-management/operations";
+import ProductsTable from "../components/products-management/ProductsTable";
+import useCheckAdminUserOnRefresh from "../hooks/useCheckAdminUserOnRefresh";
+
+const ProductsPage = () => {
+	const router = useRouter();
+	const dispatch = useDispatch();
+
+	useCheckAdminUserOnRefresh(router, dispatch);
+	return (
+		<Layout>
+			<div className="container">
+				<h2 className="heading">Products</h2>
+				<Operations />
+				<ProductsTable />
+			</div>
+		</Layout>
+	);
+};
+
+export default ProductsPage;
