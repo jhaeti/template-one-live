c642c2bdf07ade408db5ae914b9eca052dc1bcf9
Ti Jhae <jhaeti@gmail.com>
2022-07-02 19:45:38 +0000

    Updated login-pizza image to loginPizza.jpg

--- frontend/src/components/login-register/LoginBoxLeft.js
+++ frontend/src/components/login-register/LoginBoxLeft.js
@@ -3,14 +3,13 @@ import Image from "next/image";
 const LoginBoxLeft = () => (
 	<div className="login-box__left">
 		<Image
+			className="login-pizza"
 			priority="true"
-			src="/imgs/login-pizza.jpg"
+			src="/imgs/loginPizza.jpg"
 			alt="Pizza"
 			layout="intrinsic"
-			sizes="50vw"
 			width={540}
 			height={640}
-			className="login-pizza"
 		/>
 	</div>
 );
